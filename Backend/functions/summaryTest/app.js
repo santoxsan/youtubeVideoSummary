14d807afbbd180fc18bc2fada3dfa7ee2db161d0
titiloxx <21146686+titiloxx@users.noreply.github.com>
2022-09-16 01:02:47 -0300

    feat: add country code to summaryTest lambda function

--- Backend/functions/summaryTest/app.js
+++ Backend/functions/summaryTest/app.js
@@ -2,14 +2,26 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 
-exports.lambdaHandler = async ({isLongSummary,openaiAccesKey,message}) => {
+const choosePrompt=(countryCode, isLongSummary, message)=>{
+    if(countryCode === "es"){
+        return `Hacer un resumen ${isLongSummary} del siguiente texto:\n${message}\n\nEl ${isLongSummary} resumen del texto es:`
+    } 
+    else if (countryCode==="en"){
+        return `Make a ${isLongSummary} summary of the next text:\n${message}\n\nThe ${isLongSummary} summary is:`
+    }
+
+    throw new Error("Country code not supported")
+}
+
+exports.lambdaHandler = async ({isLongSummary,openaiAccesKey,message, countryCode}) => {
 
   const configuration = new Configuration({
     apiKey: openaiAccesKey,
   });
 
-  const prompt=`Make a ${isLongSummary} summary of the next text:\n${message}\n\nThe ${isLongSummary}  summary is:`
-    const response = await openai.createCompletion({
+  const prompt= choosePrompt(countryCode, isLongSummary, message);
+
+  const response = await openai.createCompletion({
   model: "text-davinci-002",
   prompt,
   temperature: 0.7,
