1a484716ef59fd052189b747dccc66ba2720f570
santiago esmoris <santiago.esmoris@gmail.com>
2022-09-16 00:49:42 -0300

    refractor: add isLongSummary variable

--- Backend/functions/getYoutubeCaption/app.js
+++ Backend/functions/getYoutubeCaption/app.js
@@ -1,11 +1,11 @@
 
 import { getContext, getCaptionOptions, getCaption } from './services.js';
 
-export const lambdaHandler = async ({url, openaiAccesKey}) => {
+export const lambdaHandler = async ({url, openaiAccesKey,isLongSummary}) => {
 
     const { id } = await getContext(url);
     const { subtitles } = await getCaptionOptions(id);
     const caption = await getCaption(subtitles[0].url);
 
-    return { message:caption,openaiAccesKey}
+    return { message:caption,openaiAccesKey,isLongSummary}
 };
--- Backend/functions/summaryTest/app.js
+++ Backend/functions/summaryTest/app.js
@@ -2,25 +2,25 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 
-exports.lambdaHandler = async (event) => {
+exports.lambdaHandler = async ({isLongSummary,openaiAccesKey,message}) => {
 
   const configuration = new Configuration({
-    apiKey: event.openaiAccesKey,
+    apiKey: openaiAccesKey,
   });
-  const openai = new OpenAIApi(configuration);
+
+  const prompt=`Make a ${isLongSummary} summary of the next text:\n${message}\n\nThe ${isLongSummary}  summary is:`
     const response = await openai.createCompletion({
   model: "text-davinci-002",
-  prompt: event.message,
+  prompt,
   temperature: 0.7,
   max_tokens: 256,
   top_p: 1,
   frequency_penalty: 0,
   presence_penalty: 0,
 });
-console.log(response)
+console.log(JSON.stringify(response.data.choices))
 return {message:response.data.choices[0].text}
 };
-
 
 
 
