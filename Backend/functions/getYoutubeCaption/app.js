9e6b23b7924ba3b2e6fccccc3319271231b5399c
titiloxx <21146686+titiloxx@users.noreply.github.com>
2022-11-06 22:39:01 -0300

    refactor: cut longs text to max 1850 words

--- Backend/functions/getYoutubeCaption/app.js
+++ Backend/functions/getYoutubeCaption/app.js
@@ -1,5 +1,7 @@
 
 import { getContext, getCaptionOptions, getCaption } from './services.js';
+const MAX_WORDS = 1850;
+
 
 export const lambdaHandler = async ({url, openaiAccessKey, isLongSummary}) => {
 
@@ -7,7 +9,14 @@ export const lambdaHandler = async ({url, openaiAccessKey, isLongSummary}) => {
     const { subtitles } = await getCaptionOptions(id);
     const firstSubtitles=subtitles[0]
     const caption = await getCaption(firstSubtitles.url);
+    // remove \n and spaces
+    const captionToAnalyzeWords = caption.split(' ');
+    const captionToAnalyzeWordsToUse = captionToAnalyzeWords.length > MAX_WORDS ? captionToAnalyzeWords.slice(0, MAX_WORDS) : captionToAnalyzeWords;
+    const captionToAnalyzeToUse = captionToAnalyzeWordsToUse.join('');
+    
+    const minifiedCaption = captionToAnalyzeToUse.replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, '');
     const countryCode = firstSubtitles.code.match(/(es)/) ? 'es' : 'en';
-    return { message:caption, openaiAccessKey, countryCode, isLongSummary}
+
+    return { message:minifiedCaption, openaiAccessKey, countryCode, isLongSummary}
 };
     
